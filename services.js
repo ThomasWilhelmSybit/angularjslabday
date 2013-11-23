21eab73969b57f803503eac5fdeaf8c3a5e76e62
rbnsybit <rbn@sybit.de>
2013-11-23 14:08:18 +0100

    no message

--- services.js
+++ services.js
@@ -3,28 +3,16 @@ var noteAppServices = angular.module('noteAppServices', []);
 noteAppServices.factory('DataService', ['$http',
   function($http){
   
-    var tags = [];  
-    var tagsflat = [];  
-  
+	var tags = [];  
     var notes = [];
-	
 	var data = {};
 	
 	data.tags = tags;
-	data.tagsflat = tagsflat;
 	data.notes = notes;
 	data.filterbytagid = {};
-	
-	data.flatentags = function(){
-					
-		angular.forEach(this.tags, function(value, key){
-		   this.tagsflat[value.id] = value;
-		});
 
-	}
-	
 	data.loadtags = function() {
-	   
+	
 	   $http.get('data/tags.json').success(function(dataloaded) {
           console.log("Loading tags");
 		  //tags = dataloaded;
@@ -32,15 +20,12 @@ noteAppServices.factory('DataService', ['$http',
 		  for (i=0;i<dataloaded.length;i++) {
              tags.push(dataloaded[i]);
           }
-		  
        });
 	}
 	
 	data.loadnotes = function() {
-		
 		 $http.get('data/notes.json').success(function(dataloaded) {
            console.log("Loading notes");
-		 
 		   notes.length = 0;
 		   
 		   for (i=0;i<dataloaded.length;i++) {
@@ -50,9 +35,16 @@ noteAppServices.factory('DataService', ['$http',
 	}
 	
 	data.getNoteById = function(id){
-		
+
 		for (i=0;i<notes.length;i++) {
           if (notes[i].id == id) return notes[i];   
+        }
+	}
+	
+	data.getTagById = function(id){
+		
+		for (i=0;i<tags.length;i++) {
+          if (tags[i].id == id) return tags[i];   
         }
 	}
 	
