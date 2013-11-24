4742b84b28e08d427f64a75188d56e8ac43823c0
Thomas Wilhelm <thomas.wilhelm@gmx.de>
2013-11-24 21:44:38 +0100

    Cleanup

--- controller.js
+++ controller.js
@@ -1,16 +1,3 @@
-Array.prototype.compare = function(testArr) {
-    if (this.length != testArr.length) return false;
-    for (var i = 0; i < testArr.length; i++) {
-        if (this[i].compare) { 
-            if (!this[i].compare(testArr[i])) return false;
-        }
-        if (this[i] !== testArr[i]) return false;
-    }
-    return true;
-}
-
-
-
 var noteAppControllers = angular.module('noteAppControllers',[]);
 
 noteAppControllers.controller('TodoCtrl', ['$scope','$http','DataService','storage', function($scope, $http, DataService, storage) {
